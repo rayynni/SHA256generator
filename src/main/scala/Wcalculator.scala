66d406f772079388455093c14017da6b9605fde9
tianyu <fa1enCrescent@gmail.com>
2025-02-07 13:07:49 +0800

    complete calculation of W.

--- src/main/scala/Wcalculator.scala
+++ src/main/scala/Wcalculator.scala
@@ -1,14 +1,18 @@
 import chisel3._
 import chisel3.util._
 
-class Wcalculator extends Module{
+class Wcalculator extends Module{ //calculate the current value of W.
   val io = IO(new Bundle {
-    val W_16 = Input(UInt(32.W)) // the underline "_" represent minus symbol "-"
-    val W_15 = Input(UInt(32.W))
-    val W_7 = Input(UInt(32.W))
-    val W_2 = Input(UInt(32.W))
+    val w_16 = Input(UInt(32.W)) // the underline "_" represent minus symbol "-"
+    val w_15 = Input(UInt(32.W))
+    val w_7 = Input(UInt(32.W))
+    val w_2 = Input(UInt(32.W))
+    val output = Output(UInt(32.W))
   })
 
-  val x = io.W_16
-  def sigma0(w : UInt): UInt = RightRotate(w, 7) ^ RightRotate(w, 18) ^ (w >> 3).asUInt
+  private def sigma0(w : UInt): UInt = RightRotate(w, 7) ^ RightRotate(w, 18) ^ (w >> 3).asUInt
+  private def sigma1(w : UInt): UInt = RightRotate(w, 17) ^ RightRotate(w, 19) ^ (w >> 10).asUInt
+
+  val w = Wire(UInt(32.W))
+  io.output := io.w_16 + sigma0(io.w_15) + io.w_7 + sigma1(io.w_2)
 }
